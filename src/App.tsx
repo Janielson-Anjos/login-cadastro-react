279c1fc7127ef6ea0be7b11a4986762fcc13f304
Janielson Anjos <72242469+Janielson-Anjos@users.noreply.github.com>
2024-10-24 19:49:59 +0000

    feat: Testando login

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,6 @@
-// import { useState } from 'react'
-// import './App.css'
-import Login from './components/login/Login';
-import Cadastro from './components/cadastro/Cadastro';
-// import './styles/global.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import LoginPage from './pages/Login'
+import LoginPage from './pages/Login';
 import CadastroPage from './pages/Cadastro';
 
 function App() {
